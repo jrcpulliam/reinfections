3f759f316b9633dd970172b33fa226184362a355
jrcpulliam <juliet@ici3d.org>
2021-11-12 22:36:57 +0200

    add figure 4 plot stuff

--- code/emp_haz_plot.R
+++ code/emp_haz_plot.R
@@ -24,73 +24,76 @@ suppressPackageStartupMessages({
 .debug <- ''
 .args <- if (interactive()) sprintf(c(
   file.path('data', 'ts_data_for_analysis.RDS'), # input
+  file.path('utils', 'emp_haz_fxn.RDS'),
+  file.path('pub.json'),
   file.path('utils', 'wave_defs.RDS'),
   file.path('utils', 'plotting_fxns.RData'),
-  file.path('output', 'ts_plot.png') # output
+  '90', 
+  file.path('output', 'emp_haz_plot.png') # output
 ), .debug[1]) else commandArgs(trailingOnly = TRUE)
 
-ts <- readRDS(.args[1])
-waves <- readRDS(.args[2])
-
-load(.args[3])
-
 target <- tail(.args, 1)
+
+ts <- readRDS(.args[1])[date < max(date) - 2]
+
+emp_haz <- readRDS(.args[2]) # Empirical hazard function
+
+configpth <- .args[3]
+attach(jsonlite::read_json(configpth))
+
+waves <- readRDS(.args[4])
+
+load(.args[5])
+
+cutoff <- as.numeric(.args[6])
+
+ehdt <- emp_haz(ts, eh$p_obs, eh$p_obs_2)
 
 geom_wave <- function(ww, dt = waves){
   geom_rect(aes(xmin = min_date, xmax = max_date, ymin = -Inf, ymax = Inf), fill = dt[wave == ww, col], alpha = .7, data = dt[wave == ww], inherit.aes = FALSE)
 }
 
-fig1a <- (ggplot(ts) 
-          + aes(x = date) 
+fig4a <- (ggplot(ehdt[!(is.na(eh_2) | is.na(eh_1))])
+          + aes(x = date)
           + geom_wave('W1')
           + geom_wave('W2')
           + geom_wave('W3')
-          + geom_point(aes(y = cnt / 10^3), size = .2, alpha = .5)
-          + geom_line(aes(y = ma_cnt / 10^3), size = 1)
-          + ylab('Primary infections')
-          + ggtitle(expression(paste('x', 10^3)))
-          + scale_x_date(NULL, date_breaks = "months", date_labels = "%b", minor_breaks = NULL, limits = ts[, range(date)])
-)
-
-fig1b <- (ggplot(ts)
-          + aes(x = date, y = elig / 10^5)
-          + geom_line(col = '7', size = 1)
-          + ylab('Population at risk\nfor reinfection')
-          + ggtitle(expression(paste('x', 10^5)))
+          + geom_line(aes(y = eh_1 / (inc_1 / eh$p_obs + inc_2 / eh$p_obs_2)), size = 1, color = 'black')
+          + geom_line(aes(y = eh_2 / (inc_1 / eh$p_obs + inc_2 / eh$p_obs_2)), size = 1, color = '3')
           + xlab('Specimen receipt date')
-          + scale_x_date(NULL, date_breaks = "months", date_labels = "%b", minor_breaks = NULL)
+          + ylab('Hazard coefficient')
 )
 
-tmp <- copy(ts)[date < min(date) + 90, reinf := NA][date < min(date) + 90, ma_reinf := NA]
-
-fig1c <- (ggplot(tmp) 
-          + aes(x = date) 
-          + geom_point(aes(y = reinf), size = .2, alpha = .5, color = '4')
-          + geom_line(aes(y = ma_reinf), size = 1, color = '4')
-          + ylab('Reinfections')
+fig4b <- (ggplot(ehdt[!(is.na(eh_2) | is.na(eh_1))])
+          + aes(x = date, y = eh_2 / eh_1)
+          + geom_line(size = 1, color = '1')
           + xlab('Specimen receipt date')
-          # + ggtitle(paste0('Positive tests of previously positive individuals (≥', cutoff ,' days later)'))
-          + scale_x_date(NULL, date_breaks = "months", date_labels = "%b", minor_breaks = NULL, name = 'Specimen receipt date')
+          + ylab('Hazard ratio')
+          + ylim(0, NA)
 )
 
-fig1 <- (fig1a / fig1b / fig1c 
+fig4 <- (fig4a / fig4b 
          + plot_annotation(tag_levels = 'A')
          & theme_minimal()
          & theme(panel.border = element_rect(colour = "black", fill = NA, size = 0.25)
-               , panel.grid.minor = element_blank())
-         & scale_x_Ms() 
+                 , panel.grid.minor = element_blank())
+         & scale_x_Ms()
          & theme(panel.grid.major.x = element_blank()
                  , axis.ticks = element_blank()
                  , panel.grid.minor.x = element_line(color = 'lightgrey', size = .5)
                  , panel.grid.minor.y = element_blank()
+                 , panel.grid.major.y = element_line(color = 'lightgrey', size = .5)
          ) 
          & geom_vline(xintercept = as.Date('2021-01-01')
                       , linetype = 2, size = .5, color = '#111111') 
-         & geom_text(aes(label = year, y = 0), data = ts[, .(year = format(date, '%Y'), date)][, .(date = min(date)), by = year], vjust = -12, hjust = 'left', nudge_x = c(0, 14))
+         & geom_text(aes(label = year, y = 0)
+                     , data = ts[, .(year = format(date, '%Y'), date)][, .(date = min(date)), by = year]
+                     , vjust = -11, hjust = 'left', nudge_x = c(0, 14)
+         )
 )
 
 if(grepl('RDS', target)){
-  saveRDS(fig1, file = target)
+  saveRDS(fig4, file = target)
 }else{
-  ggsave(fig1, filename = target, width = 7, height = 7)
+  ggsave(fig4, filename = target, width = 5.5, height = 4)
 }
