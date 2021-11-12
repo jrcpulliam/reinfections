b173b80aa08e02d39378961c5d447440642640b4
jrcpulliam <juliet@ici3d.org>
2021-11-12 21:12:45 +0200

    add data prep script for demog data

---
+++ code/prep_demog_data.R
@@ -0,0 +1,32 @@
+# This file is made available under a CC-BY-NC 4.0 International License.
+# Details of the license can be found at
+# <https://creativecommons.org/licenses/by-nc/4.0/legalcode>. 
+# 
+# Giving appropriate credit includes citation of the related publication and
+# providing a link to the repository:
+# 
+# Citation: Pulliam, JRC, C van Schalkwyk, N Govender, A von Gottberg, C Cohen,
+# MJ Groome, J Dushoff, K Mlisana, and H Moultrie. (2021) _SARS-CoV-2 reinfection
+# trends in South Africa: analysis of routine surveillance data_. _medRxiv_
+# <https://www.medrxiv.org/content/10.1101/2021.11.11.21266068>
+# 
+# Repository: <https://github.com/jrcpulliam/reinfections>
+
+suppressPackageStartupMessages({
+  require(data.table)
+})
+
+.debug <- 'data'
+.args <- if (interactive()) sprintf(c(
+  file.path('%s', 'demog_data.csv'), # input
+  file.path('%s', 'demog_data_for_display.RData') # output
+), .debug[1]) else commandArgs(trailingOnly = TRUE)
+
+dat <- data.table(read.csv(.args[1], comment.char = '#', stringsAsFactors = FALSE)) # Use to set wave dates as >15% of wave peak
+
+target <- tail(.args, 1)
+
+tab_prov <- dat[, .(no_reinf = sum(no_reinf), reinf = sum(reinf), total = sum(total)), by = province]
+tab_age_sex <- dat[, .(no_reinf = sum(no_reinf), reinf = sum(reinf), total = sum(total)), by = .(agegrp5, sex)]
+
+save(tab_prov, tab_age_sex, file = target)
